7144bfd6df32ebc085a9fb38e60b7b16a7af78fd
j-mmyni <j.ni@betclicgroup.com>
2025-03-12 15:08:21 +0100

    Fix: Update CORS configuration to allow requests from the new client URL

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,10 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true
-}));
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 
